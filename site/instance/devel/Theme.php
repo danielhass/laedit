3a5f5c9325590b4ff5b7c7bba85500575266add3
Daniel H <mail@danielhass.de>
2015-02-21 13:34:30 +0100

    Fixed checkbox position

--- site/instance/devel/Theme.php
+++ site/instance/devel/Theme.php
@@ -57,30 +57,25 @@ EOF;
 EOF;
       foreach ($attributes as $value) {
         $attr = $value->getAttribute();
+        $result .=  '<div class="attr-body"> <div class="attr-header"> '.$attr->getDisplayName().':</div>';
         if ($attr->getWidgetType() === "password")
         {
-          $result .=  '<div class="attr-body"> <div class="attr-header"> '.$attr->getDisplayName().':</div>';
           $result .=  '<input id="'.$attr->getLDAPName().'_1" class="form-control input-sm chat-input editor-input" type="password" size="25" value="'.password_placeholder.'"/><br/>';
           $result .=  '<input id="'.$attr->getLDAPName().'_2" class="form-control input-sm chat-input editor-input" type="password" size="25" value="'.password_placeholder.'"/>';
-          $result .=  '</div>';
         }
         else if ($attr->getWidgetType() === "label")
         {
-          $result .=  '<div class="attr-body"> <div class="attr-header"> '.$attr->getDisplayName().':</div>';
           $result .=  '<input id="'.$attr->getLDAPName().'" class="form-control input-sm chat-input" type="label" readonly="true" size="25" value="'.$value->getValue().'"/>';
-          $result .=  '</div>';
         }
         else if ($attr->getWidgetType() === "text")
         {
-          $result .=  '<div class="attr-body"> <div class="attr-header"> '.$attr->getDisplayName().':</div>';
           $result .=  '<input id="'.$attr->getLDAPName().'" class="form-control input-sm chat-input editor-input" type="text" size="25" value="'.$value->getValue().'"/>';
-          $result .=  '</div>';
         }
         else if ($attr->getWidgetType() === "checkbox")
         {
-          $result .=  '<div class="attr-body"> <div class="attr-header"> '.$attr->getDisplayName().':</div> <input id="'.$attr->getLDAPName().'" type="checkbox"/>';
-          $result .=  '</div>';
+          $result .=  '<input id="'.$attr->getLDAPName().'" class="editor-checkbox" type="checkbox"/>';
         }
+        $result .=  '</div>';
       }
       $result .=  "<br/><button type=\"button\" class=\"btn btn-primary login-btn\" onClick=\"workflow_continue();\">Commit changes</button>";
 
